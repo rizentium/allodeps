238f056150e1253b1de0980caf3fb54b01aeeeb6
Arif Hidayat <arif.h@bukalapak.com>
2022-05-31 16:59:12 +0700

    added flutter stetho

--- qr-mobile-vision/example/ios/Flutter/flutter_export_environment.sh
+++ qr-mobile-vision/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/arif/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/arif/Projects/pickpack-app/.dependencies/qr-mobile-vision/example"
+export "FLUTTER_APPLICATION_PATH=/home/arif/Projects/pickpack/.dependencies/qr-mobile-vision/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
